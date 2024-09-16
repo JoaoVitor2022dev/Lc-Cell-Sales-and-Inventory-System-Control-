df9eac2b1d029edd676144b23eb08700d8c2cf16
Joao Vitor <githubconta2021@gmail.com>
2024-09-16 00:26:22 -0300

    Description: finalizing the Strylo from the sales list

--- Lc Cell Sistema de Controle/br.com.project.view/FrmSalesHistory.Designer.cs
+++ Lc Cell Sistema de Controle/br.com.project.view/FrmSalesHistory.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.txtDateEnd = new System.Windows.Forms.DateTimePicker();
@@ -134,10 +136,29 @@
             // 
             // SaleTable
             // 
+            this.SaleTable.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.SaleTable.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
-            this.SaleTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SaleTable.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.RoyalBlue;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.SaleTable.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.SaleTable.ColumnHeadersHeight = 24;
+            this.SaleTable.GridColor = System.Drawing.Color.Black;
             this.SaleTable.Location = new System.Drawing.Point(12, 301);
             this.SaleTable.Name = "SaleTable";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.SaleTable.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.SaleTable.Size = new System.Drawing.Size(810, 321);
             this.SaleTable.TabIndex = 8;
             // 
