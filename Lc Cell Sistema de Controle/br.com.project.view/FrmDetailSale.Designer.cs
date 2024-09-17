b89c579a29ed6adf95c1bb862c0d04583538aae0
Joao Vitor <githubconta2021@gmail.com>
2024-09-16 23:28:33 -0300

     Description: develop as user interface for sales details

---
+++ Lc Cell Sistema de Controle/br.com.project.view/FrmDetailSale.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Lc_Cell_Sistema_de_Controle.br.com.project.view
+{
+    partial class FrmDetailSale
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SaleTable = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtClient = new System.Windows.Forms.TextBox();
+            this.textDate = new System.Windows.Forms.TextBox();
+            this.txtObservation = new System.Windows.Forms.TextBox();
+            this.textTotalSale = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SaleTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-62, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(949, 156);
+            this.panel1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(285, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(339, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Detalhes de Vendas";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textTotalSale);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtObservation);
+            this.groupBox1.Controls.Add(this.textDate);
+            this.groupBox1.Controls.Add(this.txtClient);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Location = new System.Drawing.Point(7, 160);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(810, 311);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Item de Venda";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(83, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 23);
+            this.label2.TabIndex = 33;
+            this.label2.Text = "Data: ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label9.Location = new System.Drawing.Point(66, 40);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(71, 23);
+            this.label9.TabIndex = 31;
+            this.label9.Text = "Cliente: ";
+            // 
+            // SaleTable
+            // 
+            this.SaleTable.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.SaleTable.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.SaleTable.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.RoyalBlue;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.SaleTable.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.SaleTable.ColumnHeadersHeight = 24;
+            this.SaleTable.GridColor = System.Drawing.Color.Black;
+            this.SaleTable.Location = new System.Drawing.Point(7, 493);
+            this.SaleTable.Name = "SaleTable";
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.SaleTable.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.SaleTable.Size = new System.Drawing.Size(810, 186);
+            this.SaleTable.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(26, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 23);
+            this.label3.TabIndex = 34;
+            this.label3.Text = "Observação: ";
+            // 
+            // txtClient
+            // 
+            this.txtClient.BackColor = System.Drawing.Color.White;
+            this.txtClient.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtClient.Location = new System.Drawing.Point(143, 40);
+            this.txtClient.Name = "txtClient";
+            this.txtClient.Size = new System.Drawing.Size(235, 26);
+            this.txtClient.TabIndex = 42;
+            // 
+            // textDate
+            // 
+            this.textDate.BackColor = System.Drawing.Color.White;
+            this.textDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textDate.Location = new System.Drawing.Point(143, 92);
+            this.textDate.Name = "textDate";
+            this.textDate.Size = new System.Drawing.Size(235, 26);
+            this.textDate.TabIndex = 43;
+            // 
+            // txtObservation
+            // 
+            this.txtObservation.BackColor = System.Drawing.Color.White;
+            this.txtObservation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtObservation.Location = new System.Drawing.Point(143, 145);
+            this.txtObservation.Multiline = true;
+            this.txtObservation.Name = "txtObservation";
+            this.txtObservation.Size = new System.Drawing.Size(580, 138);
+            this.txtObservation.TabIndex = 53;
+            // 
+            // textTotalSale
+            // 
+            this.textTotalSale.BackColor = System.Drawing.Color.White;
+            this.textTotalSale.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textTotalSale.Location = new System.Drawing.Point(535, 43);
+            this.textTotalSale.Name = "textTotalSale";
+            this.textTotalSale.Size = new System.Drawing.Size(188, 26);
+            this.textTotalSale.TabIndex = 55;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(410, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 23);
+            this.label4.TabIndex = 54;
+            this.label4.Text = "Total da Venda:";
+            // 
+            // FrmDetailSale
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 691);
+            this.Controls.Add(this.SaleTable);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmDetailSale";
+            this.Text = "FrmDetailSale";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SaleTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DataGridView SaleTable;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textDate;
+        private System.Windows.Forms.TextBox txtClient;
+        private System.Windows.Forms.TextBox textTotalSale;
+        private System.Windows.Forms.Label label4;
+        public System.Windows.Forms.TextBox txtObservation;
+    }
+}
--- Lc Cell Sistema de Controle/br.com.project.view/FrmSalesHistory.Designer.cs
+++ Lc Cell Sistema de Controle/br.com.project.view/FrmSalesHistory.Designer.cs
@@ -32,13 +32,13 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.txtDateEnd = new System.Windows.Forms.DateTimePicker();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label9 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtDateStart = new System.Windows.Forms.DateTimePicker();
-            this.btnSearch = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
             this.SaleTable = new System.Windows.Forms.DataGridView();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtDateStart = new System.Windows.Forms.DateTimePicker();
+            this.txtDateEnd = new System.Windows.Forms.DateTimePicker();
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.SaleTable)).BeginInit();
@@ -64,17 +64,6 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Histórico de vendas ";
             // 
-            // txtDateEnd
-            // 
-            this.txtDateEnd.CalendarFont = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDateEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDateEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.txtDateEnd.Location = new System.Drawing.Point(116, 65);
-            this.txtDateEnd.Name = "txtDateEnd";
-            this.txtDateEnd.Size = new System.Drawing.Size(231, 26);
-            this.txtDateEnd.TabIndex = 6;
-            this.txtDateEnd.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
-            // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.btnSearch);
@@ -89,17 +78,6 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Venda";
             // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label9.Location = new System.Drawing.Point(16, 65);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(94, 23);
-            this.label9.TabIndex = 31;
-            this.label9.Text = "Data Início:";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
@@ -111,28 +89,16 @@
             this.label2.TabIndex = 33;
             this.label2.Text = "Data Fim:";
             // 
-            // txtDateStart
+            // label9
             // 
-            this.txtDateStart.CalendarFont = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDateStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDateStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.txtDateStart.Location = new System.Drawing.Point(440, 66);
-            this.txtDateStart.Name = "txtDateStart";
-            this.txtDateStart.Size = new System.Drawing.Size(231, 26);
-            this.txtDateStart.TabIndex = 32;
-            // 
-            // btnSearch
-            // 
-            this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
-            this.btnSearch.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSearch.ForeColor = System.Drawing.Color.White;
-            this.btnSearch.Location = new System.Drawing.Point(689, 63);
-            this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(115, 31);
-            this.btnSearch.TabIndex = 47;
-            this.btnSearch.Text = "Pesquisa";
-            this.btnSearch.UseVisualStyleBackColor = false;
-            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label9.Location = new System.Drawing.Point(16, 65);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(94, 23);
+            this.label9.TabIndex = 31;
+            this.label9.Text = "Data Início:";
             // 
             // SaleTable
             // 
@@ -159,15 +125,49 @@
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.SaleTable.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
-            this.SaleTable.Size = new System.Drawing.Size(810, 321);
+            this.SaleTable.Size = new System.Drawing.Size(810, 389);
             this.SaleTable.TabIndex = 8;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.btnSearch.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.Location = new System.Drawing.Point(689, 63);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(115, 31);
+            this.btnSearch.TabIndex = 47;
+            this.btnSearch.Text = "Pesquisa";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtDateStart
+            // 
+            this.txtDateStart.CalendarFont = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDateStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDateStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.txtDateStart.Location = new System.Drawing.Point(440, 66);
+            this.txtDateStart.Name = "txtDateStart";
+            this.txtDateStart.Size = new System.Drawing.Size(231, 26);
+            this.txtDateStart.TabIndex = 32;
+            // 
+            // txtDateEnd
+            // 
+            this.txtDateEnd.CalendarFont = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDateEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDateEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.txtDateEnd.Location = new System.Drawing.Point(116, 65);
+            this.txtDateEnd.Name = "txtDateEnd";
+            this.txtDateEnd.Size = new System.Drawing.Size(231, 26);
+            this.txtDateEnd.TabIndex = 6;
+            this.txtDateEnd.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // FrmSalesHistory
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(834, 634);
+            this.ClientSize = new System.Drawing.Size(832, 702);
             this.Controls.Add(this.SaleTable);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.panel1);
@@ -187,12 +187,12 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DateTimePicker txtDateEnd;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DateTimePicker txtDateStart;
-        private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.DataGridView SaleTable;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DateTimePicker txtDateStart;
+        private System.Windows.Forms.DateTimePicker txtDateEnd;
     }
 }
